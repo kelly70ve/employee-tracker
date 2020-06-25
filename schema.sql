c5d217f7985df74c8f731083ae678dae4f1bcca7
Kelly Gowing <Gowing.kelly@yahoo.com>
2020-06-24 18:50:32 -0700

    created employee_db schema

---
+++ schema.sql
@@ -0,0 +1,32 @@
+DROP DATABASE IF EXISTS employee_db;
+CREATE DATABASE employee_db;
+
+USE employee_db;
+
+-- DEPARTMENT TABLE
+CREATE TABLE department (
+	id int NOT NULL AUTO_INCREMENT,
+    name VARCHAR(30) NOT NULL,
+	PRIMARY KEY (id)
+);
+
+
+--  ROLE TABLE 
+CREATE TABLE role (
+	id int NOT NULL AUTO_INCREMENT,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL(10,4),
+    department_id int,
+    PRIMARY KEY (id)
+);
+
+-- EMPLOYEE TABLE
+CREATE TABLE employee (
+	id int NOT NULL AUTO_INCREMENT,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id int,
+    manager_id int,
+    PRIMARY KEY (id)
+);
+
