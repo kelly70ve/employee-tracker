7797009b6c6eccaa8f61642e7b44e9db511d93ca
Kelly Gowing <Gowing.kelly@yahoo.com>
2020-06-28 21:07:14 -0700

    created search by manager query

--- server.js
+++ server.js
@@ -1,24 +1,19 @@
 var mysql = require("mysql");
 var inquirer = require("inquirer");
 const cTable = require('console.table');
-
 // create the connection information for the sql database
 var connection = mysql.createConnection({
   host: "localhost",
-
   // Your port; if not 3306
   port: 3306,
-
   // Your username
   user: "root",
-
   // Your password
   password: "*0penx2",
   database: "employee_db"
 });
-
 // connect to the mysql server and sql database
-connection.connect(function(err) {
+connection.connect(function (err) {
   if (err) throw err;
   // run the start function after the connection is made to prompt the user
   start();
@@ -31,50 +26,43 @@ function start() {
       type: "list",
       message: "What would you like to do?",
       choices: [
-      "View All Employees",
-      "View All Employees by Department", 
-      "View All Employees by Manager", 
-      "Add Employee",
-      "Remove Employee",
-      "Update Employee Role",
-      "Update Employee Manager",
-      "View All Roles",
-      "EXIT"
+        "View All Employees",
+        "View All Employees by Department",
+        "View All Employees by Manager",
+        "Add Employee",
+        "Remove Employee",
+        "Update Employee Role",
+        "Update Employee Manager",
+        "View All Roles",
+        "EXIT"
       ]
     })
     .then((answer) => {
-      console.log(answer)
       switch (answer.action) {
         case ("View All Employees"):
-          view('');
+          return view('');
         case ("View All Employees by Department"):
-          departments();
+          return departments();
         case ("View All Employees by Manager"):
-          console.log("View All Employees by Manager");
+          return manager();
         case ("Add Employee"):
-          console.log("Add Employee");
+          return console.log("Add Employee");
         case ("Remove Employee"):
-          console.log("Remove Employee");
+          return console.log("Remove Employee");
         case ("Update Employee Role"):
-          console.log("Update Employee Role");
+          return console.log("Update Employee Role");
         case ("Update Employee Manager"):
-          console.log("Update Employee Manager");
+          return console.log("Update Employee Manager");
         case ("View All Roles"):
-          console.log("View All Roles");
+          return console.log("View All Roles");
         case ("EXIT"):
-          connection.end(); // if end() then program breaks
+          return connection.end(); // if end() then program breaks
       }
-    }).then(start);
+    })
 }
 
-
-
-
 // VIEW 
-
 function view(where) {
-  console.log(where);
-
   connection.query(
   `SELECT
   employee.id,
@@ -86,33 +74,52 @@ function view(where) {
   FROM employee
   INNER JOIN role ON role.id = employee.id
   INNER JOIN department ON role.department_id = department.id 
-  ${where}`, function(err, res) {
+  ${where}`, function (err, res) {
     if (err) throw err;
     console.log("\n")
     console.table(res);
+    console.log("\n")
+    console.log("\n")
+    start();
   });
 }
 
-
-
 // by Department
-
 function departments() {
-  inquirer
-    .prompt({
-      name: "department",
-      type: "list",
-      message: "Which deparment would you like to view?",
-      choices: [
-      "Sales",
-      "Accounting", 
-      "Engineering", 
-      "Legal",
-      ]
-    }).then(answer => {
-      return view(`WHERE department.name = '${answer.department}'`);
-    })
+  connection.query( 
+    `SELECT name FROM department`
+    ,function (err, res) {
+      inquirer
+      .prompt({
+        name: "department",
+        type: "list",
+        message: "Which deparment would you like to view?",
+        choices: res
+      }).then(answer => {
+        return view(`WHERE department.name = '${answer.department}'`);
+      })
+    });  
 };
 
+// by Manager 
+function manager() {
+  connection.query(
+    `SELECT
+    CONCAT(first_name, ' ', last_name) AS name 
+    FROM employee
+    WHERE manager = TRUE`
+    , function (err, res) {
+      inquirer
+      .prompt({
+        name: "manager",
+        type: "list",
+        message: "Which deparment would you like to view?",
+        choices: res
+      }).then(answer => {
+        return view(`WHERE manager_name = '${answer.manager}'`);
+      })
+    })
+}
 
-//  INSERT 
+
+//  INSERT
